0511e3a9eb94a6fbe8d6073c061e5f3b4466ff69
HieuLM-AVISPL <90905279+HieuLM-AVISPL@users.noreply.github.com>
2024-08-22 14:41:28 +0700

    The PR for Haivision Media Gateway adapter (#2)
    * SYAL-draft-code draft code for the adapter
    
    * SYAL-1518 implement get device and route info
    
    * SYAL-1518 add java doc
    
    * SYAL-1518 update the Route Status
    
    * SYAL-1518 test automatic create tag with feature branch
    
    * SYAL-1518 test auto delete existing tags with same version
    
    * SYAL-1518 update the automatic creation tag in release branch
    
    * SYAL-1518 add java doc
    
    * SYAL-1518 update setter and getter for pingMode. Add Unit Test
    
    * SYAL-1518 adding validation filtering and change display protocol, route uptime

---
+++ src/main/java/com/avispl/symphony/dal/infrastructure/gateway/haivision/srt/common/metric/DeviceInfoEnum.java
@@ -0,0 +1,60 @@
+/*
+ *  Copyright (c) 2024 AVI-SPL, Inc. All Rights Reserved.
+ */
+
+package com.avispl.symphony.dal.infrastructure.gateway.haivision.srt.common.metric;
+
+/**
+ * Enum representing various device information metrics.
+ *
+ * @author Harry / Symphony Dev Team<br>
+ * Created on 8/12/2024
+ * @since 1.0.0
+ */
+public enum DeviceInfoEnum {
+	DEVICE_ID("DeviceID", "_id"),
+	TYPE("Type", "type"),
+	IP_ADDRESS("IPAddress", "ip"),
+	DEVICE_NAME("DeviceName", "name"),
+	LAST_CONNECTED("LastConnectedAt(GMT)", "lastConnectedAt"),
+	STATUS_CODE("StatusCode", "statusCode"),
+	STATUS("Status", "status"),
+	STATUS_DETAILS("StatusDetails", "statusDetails"),
+	SERIAL_NUMBER("SerialNumber", "serialNumber"),
+	FIRMWARE_VERSION("FirmwareVersion", "firmware"),
+	HAS_ADMIN_ERROR("HasAdminError", "hasAdminError"),
+	PENDING_SYNC("PendingSync", "pendingSync"),
+	LAST_CONNECTION("LastConnection", "lastConnection"),
+	;
+	private final String name;
+	private final String field;
+
+	/**
+	 * Constructor for DeviceInfoMetric.
+	 *
+	 * @param name The name representing the system information category.
+	 * @param field The field associated with the category.
+	 */
+	DeviceInfoEnum(String name, String field) {
+		this.name = name;
+		this.field = field;
+	}
+
+	/**
+	 * Retrieves {@link #name}
+	 *
+	 * @return value of {@link #name}
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * Retrieves {@link #field}
+	 *
+	 * @return value of {@link #field}
+	 */
+	public String getField() {
+		return field;
+	}
+}
